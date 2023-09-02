f8bccc3bb7a067694f7c0f33949ea6bd29b0190d
JoseBenja <jose.benjamin.mendez@hotmail.com>
2023-09-02 11:32:34 -0600

    Arquetipos Backend y frontend

--- votaciones/README.md
+++
@@ -1,2 +0,0 @@
-# Analisis-Sistemas-II
-Versionamiento del proyecto de analisis de sistemas II, Proyecto de un sistema de votaciones
--- votaciones/src/main/java/umg/analisis/sistemas/sistemavotaciones/projections/CaracteristicasVotanteProjection.java
+++
@@ -1,8 +0,0 @@
-package umg.analisis.sistemas.sistemavotaciones.projections;
-
-public interface CaracteristicasVotanteProjection {
-
-    String getEtnia();
-    Boolean getAnalfabeta();
-    String getDiscapacidad();
-}
